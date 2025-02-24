d1c9142d47b60998769bf412bc185c18f29498af
zyrab <zurakrua@gmail.com>
2025-02-24 01:20:39 +0100

    refactor: projectComponents rerganization

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,46 +1,30 @@
 import { html, DIV, H, P, IMG } from "../../services/DOMConstructor.js";
+import { onReplace } from "../../services/DOMDinamic.js";
 import { Button } from "../common/Button.js";
-import { EmbdedVideo } from "../common/EmbdedVideo.js";
+import { EmbdedVideo } from "./EmbdedVideo.js";
+import { ProjectImage } from "./ProjectImage.js";
 import { Icons8 } from "../common/Icons8.js";
 export const ProjectCard = (project) => {
-  const video = html({
-    clasS: "w-100 h-40",
-    children: [
-      "video",
-      // EmbdedVideo(
-      //   "https://www.youtube.com/embed/yq6jek2JwTw?si=tAYid23nNqzBKMs4"
-      // ),
-    ],
-  });
+  const video = EmbdedVideo("yq6jek2JwTw?si=tAYid23nNqzBKMs4");
 
   const title = H("h1", { clasS: "xxl", text: project.title });
   const date = P({ clasS: "md right", text: project.date });
   const description = P({ clasS: "md h-60", text: project.description });
   const button = Button({ text: project.buttonText, icon: project.icon });
-  const image = IMG({
-    src: "assets/numbero.png",
-    alt: project.title,
-    ID: project.id,
-    clasS: "w-100 h-100",
-  });
+
   const onImageClick = {
     event: "click",
-    callback: (e) => {
-      let image = e.target.closest(".toChange");
-      if (image) {
-        console.log(e.target);
-        image.replaceChildren();
-        image.classList.remove("toChange");
-        image.append(video);
-      }
-    },
+    callback: (e) => onReplace(e, ".toChange", video),
   };
 
   return html({
     clasS: "project-card",
     events: [onImageClick],
     children: [
-      DIV([image], "toChange w-100 h-40"),
+      DIV(
+        [ProjectImage(project, project.video ? true : false)],
+        (project.video ? "toChange" : "") + " w-100 h-50"
+      ),
       html({
         clasS: "flex col just-sb gap-1 h-60 p-1",
         children: [
--- src/components/projects/ProjectImage.js
+++ src/components/projects/ProjectImage.js
@@ -1,4 +1,43 @@
-import { IMG, DIV } from "../../services/DOMConstructor";
+import { IMG, DIV, SPAN } from "../../services/DOMConstructor.js";
 
-export const ProjectImage = ({ src, alt }) =>
-  DIV([IMG({ src, alt })], "w-100 h-100");
+export const ProjectImage = (project, video = false) => {
+  return DIV(
+    [
+      IMG({
+        src: "assets/projects/" + (project.image || "numbero-thumb.png"),
+        alt: project.title,
+        ID: project.id,
+        clasS: "w-100 h-100",
+      }),
+      video
+        ? SPAN({
+            clasS: "play-button",
+            style: {
+              background: "rgba(0, 0, 0, 0.5)",
+              width: "50px",
+              height: "50px",
+              position: "absolute",
+              borderRadius: "50%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+            children: [
+              SPAN({
+                style: {
+                  marginLeft: "5px",
+                  width: 0,
+                  height: 0,
+                  borderLeft: "8px solid transparent",
+                  borderRight: "8px solid transparent",
+                  borderBottom: "16px solid red",
+                  transform: "rotate(90deg)",
+                },
+              }),
+            ],
+          })
+        : "",
+    ],
+    "w-100 h-100 relative flex just-c align-c"
+  );
+};
