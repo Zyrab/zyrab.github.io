3968ae0a5d8fd8a04b320f2b800cc3f610a44fb7
zyrab <zurakrua@gmail.com>
2025-03-06 01:59:33 +0100

    ref: now full path is requred to display iconss properly

--- src/components/common/Icons8.js
+++ src/components/common/Icons8.js
@@ -1,13 +1,13 @@
 import { A, IMG } from "../../services/DOMConstructor.js";
 
-export const Icons8 = ({ icon, link, size = "20px", custum = "color/48/" }) => {
+export const Icons8 = ({ icon, link, size = "20px" }) => {
   return A({
     href: link,
     target: "_blank",
     clasS: "flex just-c",
     child: IMG({
       style: { width: size, height: size },
-      src: "https://img.icons8.com/" + custum + icon + ".png",
+      src: "https://img.icons8.com/" + icon + ".png",
       alt: icon,
     }),
   });
