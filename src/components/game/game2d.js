062e7a105fa7e997bcd3a8a2c00ae13f48fb2f38
zyrab <zurakrua@gmail.com>
2025-03-06 02:01:09 +0100

    ref: add proper cleanup methods

--- src/components/game/game2d.js
+++ src/components/game/game2d.js
@@ -3,27 +3,44 @@ import {
   resizeCanvas2D,
   getCursorPosition,
   addAnimation,
+  stopAnimationLoop,
 } from "./canvas2d.js";
 import { projectile } from "./projectiles2d.js";
 import { createAsteroid } from "./asteroid2d.js";
-
 export const initGame = (parent) => {
   const { canvas, ctx } = initCanvas2D("canvas2D", parent);
   canvas.style.position = "absolute";
   resizeCanvas2D(canvas, ctx);
 
-  const startX = canvas.width / 2; // Set starting position
+  const startX = canvas.width / 2;
   const startY = canvas.height;
   const projectiles = [];
-  canvas.addEventListener("click", (e) => {
-    const { x, y } = getCursorPosition(canvas, e);
-    const projetile = projectile(startX, startY, x, y, ctx, projectiles);
-    projectiles.push(projetile);
-    addAnimation(projetile, ctx);
-    console.log(projectiles.length);
-  });
+  let intervalId;
 
-  setInterval(() => {
+  const clickHandler = (e) => {
+    const { x, y } = getCursorPosition(canvas, e);
+    const projectileInstance = projectile(
+      startX,
+      startY,
+      x,
+      y,
+      ctx,
+      projectiles
+    );
+    projectiles.push(projectileInstance);
+    addAnimation(projectileInstance, ctx);
+  };
+
+  canvas.addEventListener("click", clickHandler);
+
+  intervalId = setInterval(() => {
     addAnimation(createAsteroid(startX, startY, ctx, projectiles), ctx);
-  }, 1000); // Spawns every 3 seconds
+  }, 1000);
+
+  // Cleanup function
+  return () => {
+    canvas.removeEventListener("click", clickHandler);
+    clearInterval(intervalId);
+    stopAnimationLoop();
+  };
 };
