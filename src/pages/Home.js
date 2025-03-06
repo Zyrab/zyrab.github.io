9b698aa699274a28e33ee30eca1c626af2c6ce55
zyrab <zurakrua@gmail.com>
2025-03-06 02:02:47 +0100

    ref: new home page with game

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,80 @@
-import { html, H } from "../services/DOMConstructor.js";
-import { initGame } from "../components/game/game2d.js";
-import { initstartTreck } from "../components/game/starGL.js";
+import { html, H, P, DIV, A, SWAPPER } from "../services/DOMConstructor.js";
+import { Icons8 } from "../components/common/Icons8.js";
+import { Button } from "../components/common/Button.js";
+import { Game } from "../components/game/Game.js";
+import { onClass, onShow } from "../services/DinamicDOM.js";
 export const Home = () => {
-  const game = html({
-    el: "div",
-    ID: "game",
-    style: { position: "relative", border: "1px solid black" },
-  });
-  initGame(game);
-  initstartTreck(game);
+  const texts = {
+    title: "Welcome to my space.",
+    description1:
+      "Here, you'll find my projects, thoughts, and things I'm building.",
+    description2:
+      "If you're here to connect or collaborate, feel free to reach out.",
+    email: "zyrab.dev@gmail.com",
+    emailLink: "mailto:zyrab.dev@gmail.com",
+    ideas: "I'm always up for cool ideas.",
+    gameInvite:
+      "Or if you're just passing through, why not play a quick game? I built it with passion—think you can beat my score?",
+    sourceCode: "See the game's source code here",
+  };
+
+  let game = SWAPPER({ clasS: "game" });
   return html({
     el: "section",
-    clasS: "page",
+    clasS: "flex col align-c just-c gap-2 min-h-100",
     children: [
-      game,
-      html({
-        clasS: "home-hero",
-        children: [
-          H("h2", {
-            clasS: "xxxl",
-            text: "Plan, Design, Build",
-          }),
-          H("h1", {
-            clasS: "xxl",
-            text: "I bring your Ideas to Life, with no boundaries",
-          }),
+      DIV(
+        [
+          game,
+          ,
+          H("h2", { clasS: "lg", text: texts.title }),
+          P({ clasS: "xl", text: texts.description1 }),
+          P({ clasS: "xl", text: texts.description2 }),
+          DIV(
+            [
+              Icons8({
+                icon: "/color/20/gmail-new",
+                link: texts.emailLink,
+                size: "20px",
+              }),
+              A({
+                href: texts.emailLink,
+                target: "_blank",
+                text: texts.email,
+                clasS: "md inherit italic decoration-none hover-white",
+              }),
+            ],
+            "flex gap-05 p-05"
+          ),
+          P({ clasS: "lg", text: texts.ideas }),
+          P({ clasS: "xl", text: texts.gameInvite }),
+          DIV(
+            [
+              Button({
+                text: "Fight!",
+                icon: "/flat-round/50/galaxy",
+                onClick: () => {
+                  onShow(game, Game(game));
+                  onClass("game", { add: "active", delay: 1 });
+                },
+              }),
+              Icons8({
+                icon: "/3d-fluency/94/github",
+                link: texts.sourceCode,
+                size: "20px",
+              }),
+              A({
+                href: texts.sourceCode,
+                target: "_blank",
+                text: texts.sourceCode,
+                clasS: "md inherit italic decoration-none hover-white",
+              }),
+            ],
+            "flex gap-05 p-05 align-c"
+          ),
         ],
-      }),
+        "flex col gap-1 max-w-550"
+      ),
     ],
   });
 };
