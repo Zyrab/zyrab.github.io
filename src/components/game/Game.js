34e065c16e18f56d9c1e278736d87aacb252af6c
zyrab <zurakrua@gmail.com>
2025-03-07 16:28:35 +0100

    feat: game option buttons

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -3,44 +3,57 @@ import { Button } from "../common/Button.js";
 import { onShow, onClass } from "../../services/DinamicDOM.js";
 import { initGame } from "./game2d.js";
 import { initstartTreck } from "./starGL.js";
-import { stopAnimationLoop } from "./canvas2d.js";
+
 export const Game = (placeHolder) => {
   let cleanupStartTreck, cleanupGame;
-  const paus = Button({
+
+  const pauseButton = Button({
     icon: "color/20/pause",
+    onClick: () => cleanupGame.pause(),
+  });
+
+  const resumeButton = Button({
+    icon: "color/20/play",
+    onClick: () => cleanupGame.resume(),
+  });
+
+  const restartButton = Button({
+    icon: "color/20/restart",
+    onClick: () => cleanupGame.restart(),
+  });
+
+  const closeButton = Button({
+    icon: "color/20/delete-sign--v1",
     onClick: () => {
-      stopAnimationLoop();
+      onClass("game", { remove: "active" });
+
+      setTimeout(() => {
+        cleanupStartTreck?.(); // Stop animations & WebGL
+        cleanupGame?.destroy(); // Remove event listeners & intervals
+        onShow(game, placeHolder);
+      }, 1000);
     },
   });
 
-  const closeButton = html({
-    style: { position: "absolute", top: 0, right: 0 },
-    children: [
-      Button({
-        icon: "color/20/delete-sign--v1",
-        text: "Close",
-        onClick: () => {
-          onClass("game", { remove: "active" });
-
-          setTimeout(() => {
-            cleanupStartTreck?.(); // Stop animations & WebGL
-            cleanupGame?.(); // Remove event listeners & intervals
-            onShow(game, placeHolder);
-          }, 1000);
-        },
-      }),
-      paus,
-    ],
+  const controls = html({
+    el: "div",
+    clasS: "absolute flex gap-05 top-0",
+    style: {
+      zIndex: 35,
+    },
+    children: [pauseButton, resumeButton, restartButton, closeButton],
   });
+
   const game = html({
     el: "div",
     clasS: "game",
     ID: "game",
-    children: [closeButton],
+    children: [controls],
   });
 
   cleanupStartTreck = initstartTreck(game);
   cleanupGame = initGame(game);
+  cleanupGame.start();
 
   return game;
 };
