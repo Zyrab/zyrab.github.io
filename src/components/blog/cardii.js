a57771a386b5350dbc68a82013ea312647827f7e
zyrab <zurakrua@gmail.com>
2025-07-17 18:19:17 +0200

    ref: fixing names

--- src/components/blog/cardii.js
+++ src/components/blog/cardii.js
@@ -1,41 +1,31 @@
-import { Domo } from "@zyrab/domo";
+import Domo from "@zyrab/domo";
 
-export const Card = (list) => {
+export default function createCard(list) {
   function formatDate(dateStr) {
     const [day, month, year] = dateStr.split("-").map(Number);
     const date = new Date(year, month - 1, day);
     const options = { year: "numeric", month: "short", day: "numeric" };
     return date.toLocaleDateString("en-US", options);
   }
-  const colors = [
-    "#00FFFF",
-    "#3498db",
-    "#ffd700",
-    "#ee82ee",
-    "#66BB6A",
-    "#EB4888",
-  ];
+
+  const colors = ["#00FFFF", "#3498db", "#ffd700", "#ee82ee", "#66BB6A", "#EB4888"];
   const i = Math.floor(Math.random() * colors.length);
 
   const detailUrl = `/${list.intro ? "blog" : "bugs"}/${list.slug}`;
+
   return Domo("article")
-    .cls("bg-greysh p-1 flex col gap-05 max-w-550")
+    .cls("flex col g-1 bg-greysh p-15 max-w-550")
     .child([
-      Domo("a")
-        .cls("und xl link")
-        .css({ textDecorationColor: colors[i] })
-        .attr({ href: detailUrl })
-        .txt(list.title),
+      Domo("h2").child([
+        Domo("a").cls("und xl link").css({ textDecorationColor: colors[i] }).attr({ href: detailUrl }).txt(list.title),
+      ]),
       Domo("aside")
-        .cls("flex gap-05 align-c")
+        .cls("flex g-05 ai-c")
         .child([
-          Domo("time")
-            .cls("md boolean")
-            .attr({ datetime: list.date })
-            .txt(formatDate(list.date)),
+          Domo("time").cls("md boolean").attr({ datetime: list.date }).txt(formatDate(list.date)),
           Domo("span").txt("•"),
           Domo("span").cls("md doctype").if(list.readTime).txt(list.readTime),
         ]),
       Domo("p").cls("md").if(list.intro).txt(list.intro),
     ]);
-};
+}
--- src/components/blog/psdost.js
+++ src/components/blog/psdost.js
@@ -1,27 +1,26 @@
-import { Domo, Router } from "@zyrab/domo";
+import Domo from "@zyrab/domo";
+import Router from "@zyrab/domo-router";
 import { fetchText } from "../../services/fetch.js";
 import { parseBlocks } from "../../services/markdown/parseBlocks.js";
-import { NotFound } from "./NotFound.js";
-export const Post = async (props) => {
+
+export default async function createPost(props) {
   const { slug } = props;
 
   const handleCopyCode = (e) => {
     let copy = e.target.closest(".copyCode");
     if (copy) {
       let code = copy.getAttribute("data-code");
-      navigator.clipboard
-        .writeText(code)
-        .then(() => (copy.textContent = "Copied!"));
+      navigator.clipboard.writeText(code).then(() => (copy.textContent = "Copied!"));
       setTimeout(() => (copy.textContent = "Copy"), 1000);
     }
   };
   const post = await fetchText(
     `https://raw.githubusercontent.com/Zyrab/dataZ/refs/heads/main${Router.base()}/${slug}.txt`
   );
-  const notFound = post === "404: Not Found";
+
   return Domo("article")
-    .cls("blog-post")
+    .cls("flex col g-2 py-3 px-1 lg-py-6 m-auto max-w-40 ")
+    .id("article-click")
     .on("click", handleCopyCode)
-    .child([notFound ? NotFound() : parseBlocks(post)])
-    .build();
-};
+    .child([parseBlocks(post)]);
+}
